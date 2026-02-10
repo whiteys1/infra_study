a2051c862f2e6573b53f7fb2a0fe6a6cb18d4c3b
whiteys1 <147918555+whiteys1@users.noreply.github.com>
2026-02-10 18:15:07 +0900

    백엔드 스케일업

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -41,8 +41,8 @@ resource "aws_ecs_task_definition" "backend" {
   family                   = "dev-backend"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = "512"
+  memory                   = "1024"
 
   execution_role_arn = aws_iam_role.ecs_task_execution.arn
   task_role_arn      = aws_iam_role.ecs_task.arn
